add950418df74888bee946719ba529cad8ab4c12
William Harrington <kb0iic@gmail.com>
2017-06-22 14:54:10 -0500

    Add accelerator for REDO.

--- ColorMatch/resource.h
+++ ColorMatch/resource.h
@@ -5,7 +5,6 @@
 #define IDD_ABOUTBOX                    100
 #define IDR_MAINFRAME                   128
 #define IDR_ColorMatchTYPE              130
-#define IDD_DIALOG1                     310
 #define IDD_DIALOG_OPTIONS              310
 #define IDC_BUTTON_DEFAULTS             1000
 #define IDC_EDIT_VALUE_1                1001
@@ -26,7 +25,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        312
-#define _APS_NEXT_COMMAND_VALUE         32779
+#define _APS_NEXT_COMMAND_VALUE         32782
 #define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
